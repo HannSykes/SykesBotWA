87065ed02ca87fa22e9160a0c72239e80a1ebac2
HannSykes <91818272+HannSykes@users.noreply.github.com>
2021-11-08 00:44:33 +0700

    HannSykes

---
+++ IlhanSykes/infoupdate.js
@@ -0,0 +1,46 @@
+[ 
+{"ilhansykes" : "⚡ *SYKES BOT UPDATE* ⚡
+```Version : 7.0 beta```
+```Update Date : Monday, 10 Oktober 2021 | 23:00 WIB```
+```Total Features : 265 ```
+```Error Features : 0 ```
+
+╭─────≽「 *WHATS NEW?* 」
+┴
+┠≽ [NEW] BAN & UNBAN USER
+┠≽ [NEW] CUSTOM TEXT WELCOME
+┠≽ [NEW] PLAY AND YT DOWNLOAD V2
+┠≽ [NEW] VOTING GROUP
+┠≽ [NEW] COGAN & CECAN
+┠≽ [NEW] DARKJOKES & MEME V2
+┠≽ [NEW] TOURL
+┠≽ [NEW] REVOKE LINK GROUP
+┠≽ [NEW] LISTVN
+┠≽ [NEW] MENU LOCATION
+┠≽ [NEW] MEME GENERATOR
+┠≽ [NEW] AFK
+┠≽ [NEW] BUG STORY
+┠≽ [NEW] STORYWA
+┠≽ [NEW] ANTI DELETE
+┠≽ [NEW] ANYMORE.
+┬
+╰────────────────────────
+
+͏͏͏͏͏͏͏͏͏͏͏͏͏͏╭─────≽「 *INFORMATION* 」
+┴
+┠≽ Jika menemukan bug silahkan lapor ya!
+┠≽ Untuk Sewa BOT silahkan chat owner ya!
+┬
+╰────────────────────────
+
+*CARA LAPOR BUG?*
+ketik #laporbug [text] atau #report [text]
+Contoh :
+#laporbug kak fitur pinterest ga respon!
+
+
+
+Arigathanks :))
+
+~ HannSykes"}
+]
